de1a345b47e0c51725dfbaf0a574dd1e5586acf9
Søren <soerensrasmussen@gmail.com>
2013-11-13 21:16:26 +0100

    GUI, generateName method and display team

--- FootballManagerGame/FootballManagerGame/MainWindow.xaml.cs
+++ FootballManagerGame/FootballManagerGame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,29 @@ namespace FootballManagerGame
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string generateName()
+        {
+            List<string> firstNames = new List<string>(File.ReadAllLines("firstNames.txt"));
+            List<string> secondNames = new List<string>(File.ReadAllLines("secondNames.txt"));
+            Random rand = new Random();
+
+            return firstNames[rand.Next(0, firstNames.Count)] + " " + secondNames[rand.Next(0, secondNames.Count)];
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            generateTeam();
+        }
+
+        private void generateTeam()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Player player = new Player(generateName(), 0, clubnameTextBox.Text, 0d, true);
+                teamTextBlock.Text += player.ToString() + "\n";
+            }
         }
     }
 }
