9bc28e1a42e87379524c042061c63478c7698b9b
Munk91 <Mikkelbendixmunk@gmail.com>
2013-11-13 20:37:11 +0100

    Class Creation

---
+++ FootballManagerGame/FootballManagerGame/IPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManagerGame
+{
+    public interface IPlayer
+    {
+        string Name { get; set; }
+        string Nationality { get; set; }
+        string Foot { get; set; }
+        string Club { get; set; }
+        string Value { get; set; }
+        string CareerInjuries { get; set; }
+        int Age { get; set; }
+        int Height { get; set; }
+        int Weight { get; set; }
+        DateTime DateOfBirth { get; set; }
+        bool Active { get; set; }
+    }
+}
---
+++ FootballManagerGame/FootballManagerGame/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballManagerGame
+{
+    public class Player
+    {
+        private string _name;
+        private string _club;
+        private int _age;
+        private double _value;
+        private bool _active;
+
+        public Player(string name, int age, string club, double value, bool active)
+        {
+            Name = name;
+            Age = age;
+            Club = club;
+            Value = value;
+            Active = active;
+        }
+
+        string Name { get; set; }
+        string Club { get; set; }
+        double Value { get; set; }
+        int Age { get; set; }
+        bool Active { get; set; }
+    }
+}
